5830a68765036cd3bda71326a7f3ef1a1258e652
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-07-11 21:40:38 -0500

    added GET route

--- server.js
+++ server.js
@@ -23,3 +23,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json()); 
+
+app.get('/', (req, res)=>{
+    db.collection('ClusterTODO').find().toArray()
+    .then(data => {
+        res.render('index.ejs', {items: data})
+    })
+})
+.catch(error => console.error(error)); 
